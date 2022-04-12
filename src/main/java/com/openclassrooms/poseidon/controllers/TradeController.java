93917aa3d8651791441793e141a16b76a98d8510
Elien <elienferre@yahoo.fr>
2022-04-12 14:59:30 +0200

    Added Trade controller features and its dependencies

--- src/main/java/com/openclassrooms/poseidon/controllers/TradeController.java
+++ src/main/java/com/openclassrooms/poseidon/controllers/TradeController.java
@@ -1,6 +1,10 @@
 package com.openclassrooms.poseidon.controllers;
 
+import com.openclassrooms.poseidon.controllers.rest.TradeRestController;
 import com.openclassrooms.poseidon.domain.Trade;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -9,46 +13,123 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 
+/**
+ * This class is the controller in charge of displaying views for everything that concerns
+ * trades.
+ */
 @Controller
+@RequestMapping("/trade")
 public class TradeController {
-    // TODO: Inject Trade service
 
-    @RequestMapping("/trade/list")
+    private static final Logger LOGGER = LogManager.getLogger(TradeController.class);
+
+    @Autowired
+    private TradeRestController tradeRestController;
+
+    /**
+     * This method is used to display all trades in a list.
+     *
+     * @param model is the model that display the page correctly.
+     * @return is a string path where to find the view for this controller's method.
+     */
+    @GetMapping("/list")
     public String home(Model model)
     {
-        // TODO: find all Trade, add to model
+        LOGGER.info("Fetching /trade/list...");
+        model.addAttribute("trades", tradeRestController.getAll());
         return "trade/list";
     }
 
-    @GetMapping("/trade/add")
-    public String addUser(Trade bid) {
+    /**
+     * This method is used to add a trade in database.
+     *
+     * @param trade is the trade Object used to display the page correctly.
+     * @return is a string path where to find the view for this controller's method.
+     */
+    @GetMapping("/add")
+    public String addUser(Trade trade) {
+        LOGGER.info("Fetching /trade/add...");
         return "trade/add";
     }
 
-    @PostMapping("/trade/validate")
+    /**
+     * This method is used to validate a new trade before adding it to database.
+     *
+     * @param trade is the trade Object in validation before add.
+     * @param result is the BindingResult object that checks if errors are present in trade object.
+     * @param model is the model that display the page correctly.
+     * @return is a string path where to find the view for this controller's method.
+     */
+    @PostMapping("/validate")
+    @Transactional
     public String validate(@Valid Trade trade, BindingResult result, Model model) {
-        // TODO: check data valid and save to db, after saving return Trade list
+        LOGGER.info("Fetching /trade/validate...");
+        LOGGER.info("Validating entries...");
+        if (!result.hasErrors()) {
+            LOGGER.info("Entries validated...");
+            model.addAttribute("trades", tradeRestController.getAll());
+            tradeRestController.create(trade);
+            return "redirect:/trade/list";
+        }
         return "trade/add";
     }
 
-    @GetMapping("/trade/update/{id}")
+    /**
+     * This method is used to display the update page of an existing trade before updating it to database.
+     *
+     * @param id is the trade's id that is supposed to be updated.
+     * @param model is the model that display the page correctly.
+     * @return is a string path where to find the view for this controller's method.
+     */
+    @GetMapping("/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        // TODO: get Trade by Id and to model then show to the form
+        LOGGER.info("Fetching /trade/update/" + id);
+        Trade trade = tradeRestController.getById(id).orElseThrow(
+          () -> new NullPointerException("No trade found with this id (" + id + ")")
+        );
+        model.addAttribute("trade", trade);
         return "trade/update";
     }
 
-    @PostMapping("/trade/update/{id}")
+    /**
+     * This method is used to validate and update an existing trade into database.
+     *
+     * @param id is the trade's id  that is supposed to be updated.
+     * @param trade is the trade object to update through this method.
+     * @param result is the BindingResult object that checks if errors are present in trade object.
+     * @param model is the model that display the page correctly.
+     * @return is a string path where to find the view for this controller's method.
+     */
+    @PostMapping("/update/{id}")
+    @Transactional
     public String updateTrade(@PathVariable("id") Integer id, @Valid Trade trade,
                              BindingResult result, Model model) {
-        // TODO: check required fields, if valid call service to update Trade and return Trade list
+        LOGGER.info("Updating through /trade/update/" + id);
+        LOGGER.info("Validating entries...");
+        if (result.hasErrors()){
+            return "trade/update";
+        }
+        LOGGER.info("Entries validated...");
+        tradeRestController.update(trade);
+        model.addAttribute("trades", tradeRestController.getAll());
         return "redirect:/trade/list";
     }
 
-    @GetMapping("/trade/delete/{id}")
+    /**
+     * This method is used to delete an existing trade from database.
+     *
+     * @param id is the trade's id that is supposed to be deleted.
+     * @param model is the model that display the page correctly.
+     * @return is a string path where to find the view for this controller's method.
+     */
+    @GetMapping("/delete/{id}")
+    @Transactional
     public String deleteTrade(@PathVariable("id") Integer id, Model model) {
-        // TODO: Find Trade by Id and delete the Trade, return to Trade list
+        LOGGER.info("Fetching /trade/delete/" + id);
+        tradeRestController.delete(id);
         return "redirect:/trade/list";
     }
 }
---
+++ src/main/java/com/openclassrooms/poseidon/controllers/rest/TradeRestController.java
@@ -0,0 +1,158 @@
+package com.openclassrooms.poseidon.controllers.rest;
+
+import com.openclassrooms.poseidon.domain.Trade;
+import com.openclassrooms.poseidon.services.TradeService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * This class is the REST Controller to communicate with database through JSON files
+ * with trade objects.
+ */
+@RestController
+@RequestMapping("${api.ver}" + "/trade")
+public class TradeRestController {
+
+  private static final Logger LOGGER = LogManager.getLogger(TradeRestController.class);
+
+  @Autowired
+  private TradeService tradeService;
+
+
+  /**
+   * this method is one of the Exception Handler to display specific exceptions from
+   * this controller.
+   *
+   * @param ex is the exception handled here
+   * @return a map with all errors stacked in this exception.
+   */
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public Map<String, String> handleValidationExceptions(
+    MethodArgumentNotValidException ex) {
+    Map<String, String> errors = new HashMap<>();
+    ex.getBindingResult().getAllErrors().forEach(
+      (error) -> {
+        String fieldName = ((FieldError) error).getField();
+        String errorMessage = error.getDefaultMessage();
+        errors.put(fieldName, errorMessage);
+      }
+    );
+    return errors;
+  }
+
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ExceptionHandler(IllegalArgumentException.class)
+  public String handleValidationExceptions(
+    IllegalArgumentException ex) {
+    return ex.getLocalizedMessage();
+  }
+
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ExceptionHandler(HttpMessageNotReadableException.class)
+  public String handleValidationExceptions(
+    HttpMessageNotReadableException ex) {
+    return "The json you provided cannot be parsed. Some variable does not match the scheme.";
+  }
+
+  /**
+   * this method is one of the Exception Handler to display specific exceptions from
+   * this controller.
+   *
+   * @param ex is the exception handled here
+   * @return a string with the error contained in this exception.
+   */
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  @ExceptionHandler(NullPointerException.class)
+  public String handleValidationExceptions(
+    NullPointerException ex) {
+    return ex.getLocalizedMessage();
+  }
+
+  /**
+   * This method is used to display all trades in a list.
+   *
+   * @return the iterable of all trades contained in database.
+   */
+  @GetMapping("/list")
+  public Iterable<Trade> getAll() {
+    LOGGER.info("API Request -> get all trades...");
+    return tradeService.getAll();
+  }
+
+  /**
+   * This method is used to add a new trade into database.
+   *
+   * @param trade is the Json body of the trade you want to add.
+   * @return the confirmation message that you correctly added the bid.
+   */
+  @PostMapping("/add")
+  @Transactional
+  public ResponseEntity<String> create(@Valid @RequestBody Trade trade) {
+    LOGGER.info("API Request -> saving trade : ");
+    LOGGER.info(trade.toString());
+    Trade savedTrade = tradeService.save(trade);
+    LOGGER.info("Trade saved successfully");
+    return ResponseEntity.ok("Successfully created with id : " + savedTrade.getTradeId() + ".");
+  }
+
+  /**
+   * This method is used to recover a specific trade thanks to its id.
+   *
+   * @param id is the trade's id you are looking for.
+   * @return an optional trade object
+   */
+  @GetMapping("/list/{id}")
+  public Optional<Trade> getById(@PathVariable Integer id) {
+    LOGGER.info("API Request -> get trade with Id : " + id + "...");
+    return tradeService.findById(id);
+  }
+
+  /**
+   * This method is used to update an existing trade from database.
+   *
+   * @param trade is the curve point you want to modify.
+   * @return the confirmation message that you correctly updated the trade.
+   */
+  @PutMapping("/update")
+  @Transactional
+  public ResponseEntity<String> update(@Valid @RequestBody Trade trade) {
+    LOGGER.info("API Request -> updating trade : ");
+    LOGGER.info(trade.toString());
+    Trade tradeFromDB = tradeService.findById(trade.getTradeId()).orElseThrow(
+      () -> new NullPointerException("No trade found with this id (" + trade.getTradeId() + ")")
+    );
+    tradeService.update(trade, tradeFromDB);
+    LOGGER.info("Trade updated successfully !");
+    return ResponseEntity.ok("Trade successfully updated !");
+  }
+
+  /**
+   * This method is used to delete a trade from database.
+   *
+   * @param id is the id of the trade you want to delete.
+   * @return the confirmation message of deletion.
+   */
+  @DeleteMapping("/delete")
+  @Transactional
+  public ResponseEntity<String> delete(@RequestParam Integer id) {
+    LOGGER.info("API Request -> deleting trade with id : " + id);
+    Trade tradeToDelete = tradeService.findById(id)
+      .orElseThrow(() -> new NullPointerException("No trade with id " + id + " exists in DB."));
+    tradeService.delete(tradeToDelete);
+    return ResponseEntity.ok("Trade with id " + id + " has been deleted successfully.");
+  }
+}
