8a2809871a1c0f843927c26217281fa4d30b9432
Elien <elienferre@yahoo.fr>
2022-04-13 11:44:06 +0200

    user feature organised with rest controller and service

--- src/main/java/com/openclassrooms/poseidon/controller/UserController.java
+++ src/main/java/com/openclassrooms/poseidon/controller/UserController.java
@@ -1,13 +1,10 @@
 package com.openclassrooms.poseidon.controller;
 
-import com.openclassrooms.poseidon.configuration.SpringSecurityConfig;
+import com.openclassrooms.poseidon.controller.rest.UserRestController;
 import com.openclassrooms.poseidon.domain.User;
-import com.openclassrooms.poseidon.repository.UserRepository;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -16,81 +13,122 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 
-// TODO : Reprendre le code pour intégrer un UserService.
+/**
+ * This class is the controller in charge of displaying views for everything that concerns
+ * users.
+ */
 @Controller
+@RequestMapping("/user")
 public class UserController {
   @Autowired
-  private UserRepository userRepository;
-
-  @Autowired
-  private SpringSecurityConfig springSecurityConfig;
+  private UserRestController userRestController;
 
   private Logger LOGGER = LogManager.getLogger(UserController.class);
 
-  @GetMapping("/user/list/index")
-  public String test(){
-    return "test";
-  }
-
-  @RequestMapping("/user/list")
+  /**
+   * This method is used to display all users in a list.
+   *
+   * @param model is the model that display the page correctly.
+   * @return is a string path where to find the view for this controller's method.
+   */
+  @GetMapping("/list")
   public String home(Model model) {
-    model.addAttribute("users", userRepository.findAll());
+    LOGGER.info("Fetching /user/list...");
+    model.addAttribute("users", userRestController.getAll());
     return "user/list";
   }
 
-  @GetMapping("/user/add")
+  /**
+   * This method is used to add a user in database.
+   *
+   * @param user is the user Object used to display the page correctly.
+   * @return is a string path where to find the view for this controller's method.
+   */
+  @GetMapping("/add")
   public String addUser(User user) {
     LOGGER.info("Fetching /user/add...");
     return "user/add";
   }
 
-  @PostMapping("/user/validate")
+  /**
+   * This method is used to validate a new user before adding it to database.
+   *
+   * @param user   is the user Object in validation before add.
+   * @param result is the BindingResult object that checks if errors are present in user object.
+   * @param model  is the model that display the page correctly.
+   * @return is a string path where to find the view for this controller's method.
+   */
+  @PostMapping("/validate")
+  @Transactional
   public String validate(@Valid User user, BindingResult result, Model model) {
-    LOGGER.info("Fetching /user/validate to check if user adding is valid...");
+    LOGGER.info("Fetching /user/validate...");
+    LOGGER.info("Validating entries...");
     if (!result.hasErrors()) {
-      BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-      user.setPassword(encoder.encode(user.getPassword()));
-      userRepository.save(user);
-      model.addAttribute("users", userRepository.findAll());
+      LOGGER.info("Entries validated...");
+      model.addAttribute("users", userRestController.getAll());
+      userRestController.create(user);
       return "redirect:/user/list";
     }
     return "user/add";
   }
 
-  // TODO : ALWAYS CHECK THAT PRINCIPAL IS THE SAME USER YOU WANT TO UPDATE
-  @GetMapping("/user/update/{id}")
+  /**
+   * This method is used to display the update page of an existing user before updating it to database.
+   *
+   * @param id    is the user's id that is supposed to be updated.
+   * @param model is the model that display the page correctly.
+   * @return is a string path where to find the view for this controller's method.
+   */
+  @GetMapping("/update/{id}")
   public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-    LOGGER.info("Fetching /user/update/" + id + "...");
-    User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+    LOGGER.info("Fetching /user/update/" + id);
+    User user = userRestController.getById(id).orElseThrow(
+      () -> new NullPointerException("No user found with this id (" + id + ")")
+    );
+    //Blank password on form page
     user.setPassword("");
     model.addAttribute("user", user);
     return "user/update";
   }
 
-  @PostMapping("/user/update/{id}")
+  /**
+   * This method is used to validate and update an existing user into database.
+   *
+   * @param id     is the user's id  that is supposed to be updated.
+   * @param user   is the user object to update through this method.
+   * @param result is the BindingResult object that checks if errors are present in user object.
+   * @param model  is the model that display the page correctly.
+   * @return is a string path where to find the view for this controller's method.
+   */
+  @PostMapping("/update/{id}")
+  @Transactional
   public String updateUser(@PathVariable("id") Integer id, @Valid User user,
                            BindingResult result, Model model) {
-    LOGGER.info("Posting on /user/update/" + id + " to update user n°" + id + "...");
+    LOGGER.info("Updating through /user/update/" + id);
+    LOGGER.info("Validating entries...");
     if (result.hasErrors()) {
       return "user/update";
     }
-
-    PasswordEncoder encoder = springSecurityConfig.passwordEncoder();
-    user.setPassword(encoder.encode(user.getPassword()));
-    user.setId(id);
-    userRepository.save(user);
-    model.addAttribute("users", userRepository.findAll());
+    LOGGER.info("Entries validated...");
+    userRestController.update(user);
+    model.addAttribute("users", userRestController.getAll());
     return "redirect:/user/list";
   }
 
-  @GetMapping("/user/delete/{id}")
+  /**
+   * This method is used to delete an existing user from database.
+   *
+   * @param id    is the user's id that is supposed to be deleted.
+   * @param model is the model that display the page correctly.
+   * @return is a string path where to find the view for this controller's method.
+   */
+  @GetMapping("/delete/{id}")
   public String deleteUser(@PathVariable("id") Integer id, Model model) {
-    LOGGER.info("Fetching on /user/delete/" + id + " to delete user n°" + id + "...");
-    User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
-    userRepository.delete(user);
-    model.addAttribute("users", userRepository.findAll());
+    LOGGER.info("Fetching /user/delete/" + id);
+    userRestController.delete(id);
     return "redirect:/user/list";
   }
 }
---
+++ src/main/java/com/openclassrooms/poseidon/controller/rest/UserRestController.java
@@ -0,0 +1,160 @@
+package com.openclassrooms.poseidon.controller.rest;
+
+import com.openclassrooms.poseidon.domain.User;
+import com.openclassrooms.poseidon.service.UserService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * This class is the REST Controller to communicate with database through JSON files
+ * with user objects.
+ */
+@RestController
+@RequestMapping("${api.ver}" + "/user")
+public class UserRestController {
+
+  private static final Logger LOGGER = LogManager.getLogger(UserRestController.class);
+
+  @Autowired
+  private UserService userService;
+
+
+  /**
+   * this method is one of the Exception Handler to display specific exceptions from
+   * this controller.
+   *
+   * @param ex is the exception handled here
+   * @return a map with all errors stacked in this exception.
+   */
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public Map<String, String> handleValidationExceptions(
+    MethodArgumentNotValidException ex) {
+    Map<String, String> errors = new HashMap<>();
+    ex.getBindingResult().getAllErrors().forEach(
+      (error) -> {
+        String fieldName = ((FieldError) error).getField();
+        String errorMessage = error.getDefaultMessage();
+        errors.put(fieldName, errorMessage);
+      }
+    );
+    return errors;
+  }
+
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ExceptionHandler(IllegalArgumentException.class)
+  public String handleValidationExceptions(
+    IllegalArgumentException ex) {
+    return ex.getLocalizedMessage();
+  }
+
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ExceptionHandler(HttpMessageNotReadableException.class)
+  public String handleValidationExceptions(
+    HttpMessageNotReadableException ex) {
+    return "The json you provided cannot be parsed. Some variable does not match the scheme.";
+  }
+
+  /**
+   * this method is one of the Exception Handler to display specific exceptions from
+   * this controller.
+   *
+   * @param ex is the exception handled here
+   * @return a string with the error contained in this exception.
+   */
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  @ExceptionHandler(NullPointerException.class)
+  public String handleValidationExceptions(
+    NullPointerException ex) {
+    return ex.getLocalizedMessage();
+  }
+
+  /**
+   * This method is used to display all users in a list.
+   *
+   * @return the iterable of all users contained in database.
+   */
+  @GetMapping("/list")
+  public Iterable<User> getAll() {
+    LOGGER.info("API Request -> get all users...");
+    return userService.getAll();
+  }
+
+  /**
+   * This method is used to add a new user into database.
+   *
+   * @param user is the Json body of the user you want to add.
+   * @return the confirmation message that you correctly added the bid.
+   */
+  @PostMapping("/add")
+  @Transactional
+  public ResponseEntity<String> create(@Valid @RequestBody User user) {
+    LOGGER.info("API Request -> saving user : ");
+    LOGGER.info(user.toString());
+    User savedUser = userService.save(user);
+    LOGGER.info("User saved successfully");
+    return ResponseEntity.ok("Successfully created with id : " + savedUser.getId() + ".");
+
+  }
+
+  /**
+   * This method is used to recover a specific user thanks to its id.
+   *
+   * @param id is the user's id you are looking for.
+   * @return an optional user object
+   */
+  @GetMapping("/list/{id}")
+  public Optional<User> getById(Integer id) {
+    LOGGER.info("API Request -> get user with Id : " + id + "...");
+    return userService.findById(id);
+  }
+
+  /**
+   * This method is used to update an existing user from database.
+   *
+   * @param user is the curve point you want to modify.
+   * @return the confirmation message that you correctly updated the user.
+   */
+  @PutMapping("/update")
+  @Transactional
+  public ResponseEntity<String> update(@Valid @RequestBody User user) {
+    LOGGER.info("API Request -> updating user : ");
+    LOGGER.info(user.toString());
+    User userFromDB = userService.findById(user.getId()).orElseThrow(
+      () -> new NullPointerException("No user found with this id (" + user.getId() + ")")
+    );
+    userService.update(user, userFromDB);
+    LOGGER.info("User updated successfully !");
+    return ResponseEntity.ok("User successfully updated !");
+  }
+
+  /**
+   * This method is used to delete a user from database.
+   *
+   * @param id is the id of the user you want to delete.
+   * @return the confirmation message of deletion.
+   */
+  @DeleteMapping("/delete")
+  @Transactional
+  public ResponseEntity<String> delete(@RequestParam Integer id) {
+    LOGGER.info("API Request -> deleting user with id : " + id);
+    User userToDelete = userService.findById(id)
+      .orElseThrow(() -> new NullPointerException("No user with id " + id + " exists in DB."));
+    userService.delete(userToDelete);
+    return ResponseEntity.ok("User with id " + id + " has been deleted successfully.");
+
+  }
+}
