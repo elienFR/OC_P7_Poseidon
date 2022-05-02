a13a4bd2d3d3b328200e96004df75950a28223c4
Elien <elienferre@yahoo.fr>
2022-05-02 10:35:50 +0200

    javadoc on TradeService

--- src/main/java/com/openclassrooms/poseidon/service/TradeService.java
+++ src/main/java/com/openclassrooms/poseidon/service/TradeService.java
@@ -14,6 +14,11 @@ import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
+/**
+ *
+ * This class is a CRUD service for trade objects
+ *
+ */
 @Service
 @Transactional
 public class TradeService {
@@ -23,11 +28,25 @@ public class TradeService {
   @Autowired
   private TradeRepository tradeRepository;
 
+  /**
+   * This method is used to display all trade in iterables.
+   *
+   * @return is an iterable containing all trade Object.
+   */
   public Iterable<Trade> getAll() {
     LOGGER.info("Contacting DB to get all trades...");
     return tradeRepository.findAll();
   }
 
+  /**
+   * This method is used to save a NEW trade in the database, and it also adds the creation
+   * date to the object.
+   * It checks that the object you want to save DOES have a null id.
+   * This null property means the object does not exist in DB.
+   *
+   * @param trade is the trade object you want to save.
+   * @return the saved trade
+   */
   public Trade save(Trade trade) {
     LOGGER.info("Contacting DB to save trade...");
     //In case the saving is a new trade, then a date is added to its CreationDate attribute
@@ -43,11 +62,24 @@ public class TradeService {
     return tradeRepository.save(trade);
   }
 
+  /**
+   * This method is used to find a specific trade object from DB thanks to its id.
+   *
+   * @param id is the id of the trade you want to retrieve form DB.
+   * @return an optional trade Object.
+   */
   public Optional<Trade> findById(Integer id) {
     LOGGER.info("Contacting DB to find trade with id : " + id);
     return tradeRepository.findById(id);
   }
 
+  /**
+   * This method is used to update an EXISTING trade object.
+   *
+   * @param modifiedTrade is the object that is going to overwrite the one in DB.
+   * @param tradeToUpdate is the object from db to be updated. (the repo that connect to db will basically just check this object id).
+   * @return the updated trade object
+   */
   public Trade update(Trade modifiedTrade, Trade tradeToUpdate) {
     LOGGER.info("Contacting DB to update trade...");
     if (modifiedTrade.getTradeId() != tradeToUpdate.getTradeId()) {
@@ -62,6 +94,11 @@ public class TradeService {
     return tradeRepository.save(tradeToUpdate);
   }
 
+  /**
+   * This method is used to delete an existing trade from database.
+   *
+   * @param tradeToDelete is the deleted trade.
+   */
   public void delete(Trade tradeToDelete) {
     LOGGER.info("Contacting DB to delete trade : " + tradeToDelete.toString());
     tradeRepository.delete(tradeToDelete);
